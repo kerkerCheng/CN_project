49abc2a31f3b7cdcc5e045ede862056baedbb6d8
kerkercheng <c3035281@gmail.com>
2018-07-01 15:42:12 +0800

    change the quality OK

--- client.py
+++ client.py
@@ -13,9 +13,8 @@ client_TCP.connect((TCP_HOST, TCP_PORT))
 
 spilit = 1
 img_size = 640*360*3
-jpg_size = 15600
+jpg_size = 20000
 packet_size = int(jpg_size/spilit)
-frame_byte = b''
 
 clock = time.time()
 packet_num = 1
@@ -26,21 +25,26 @@ print("Wait for image....")
 while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    elif cv2.waitKey(1) & 0xFF == ord('z'):
+        client_TCP.send('mid\n'.encode('utf-8'))
+    elif cv2.waitKey(1) & 0xFF == ord('x'):
+        client_TCP.send('mid-low\n'.encode('utf-8'))
+    elif cv2.waitKey(1) & 0xFF == ord('c'):
+        client_TCP.send('low\n'.encode('utf-8'))
+    elif cv2.waitKey(1) & 0xFF == ord('v'):
+        client_TCP.send('lowlow\n'.encode('utf-8'))
 
-    segment, address = s.recvfrom(packet_size)
-    frame_byte = segment
+    frame_byte, address = s.recvfrom(packet_size)
 
     jpg_recover = np.frombuffer(frame_byte, np.uint8)
     frame_recover = cv2.imdecode(jpg_recover, cv2.IMREAD_COLOR)
     if frame_recover is None:
-        frame_byte = b''
         continue
-    cv2.putText(frame_recover, str(len(frame_byte)), (10, 10), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
-    cv2.putText(frame_recover, str(fps), (10, 20), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+    cv2.putText(frame_recover, str(len(frame_byte)), (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+    cv2.putText(frame_recover, str(fps), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
     cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('frame', 1280, 720)
     cv2.imshow('frame', frame_recover)
-    frame_byte = b''
 
     packet_num += 1
     if (time.time() - clock) > 1:
@@ -49,3 +53,4 @@ while True:
         clock = time.time()
 
 cv2.destroyAllWindows()
+client_TCP.close()
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import socket
 import threading
 import numpy as np
+import time
 
 HOST, PORT = "127.0.0.1", 666
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -14,55 +15,88 @@ server.listen(5)
 
 print("listening on %s : %d" % (recv_HOST, recv_PORT))
 
-
-def handle_client(client_socket):
-    request = client_socket.recv(1024)
-    print("Received: %s" % request)
-    client_socket.send("ACK!")
-    client_socket.close()
-
-
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 360)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
 
-spilit = 1
-img_size = 360*640*3
-jpg_size = 15600
-packet_size = int(jpg_size / spilit)
-
-
-state = 0
 
 def main():
-    
+    t = threading.Thread(target=do_stream, args=((40, 20000),))
+    t.start()
+    client, addr = server.accept()
+    print("Acepted connection from : %s:%d" % (addr[0], addr[1]))
+    while True:
+        recv_byte = client.recv(1024)
+        request = recv_byte.decode('utf-8').split('\n')[0]
+
+        if request == 'mid':
+            print('mid----')
+            parameter = (40, 20000)
+            t.do_run = False
+            t.join()
+            t = threading.Thread(target=do_stream, args=(parameter,))
+            t.start()
+        elif request == 'mid-low':
+            print('mid-low----')
+            parameter = (30, 15000)
+            t.do_run = False
+            t.join()
+            t = threading.Thread(target=do_stream, args=(parameter,))
+            t.start()
+        elif request == 'low':
+            print('low----')
+            parameter = (20, 12000)
+            t.do_run = False
+            t.join()
+            t = threading.Thread(target=do_stream, args=(parameter,))
+            t.start()
+        elif request == 'lowlow':
+            print('lowlow----')
+            parameter = (10, 8000)
+            t.do_run = False
+            t.join()
+            t = threading.Thread(target=do_stream, args=(parameter,))
+            t.start()
 
 
-while True:
+def do_stream(parameter):
+    print('start thread')
+    quality, packet_size = parameter
+    t = threading.current_thread()
+    while getattr(t, "do_run", True):
+        video_streaming(quality, packet_size)
+    print("stop thread")
+
+
+def video_streaming(quality, packet_size):
+    spilit = 1
+    img_size = 360 * 640 * 3
+    jpg_size = packet_size
+    packet_size = int(jpg_size / spilit)
 
     ret, frame = cap.read()
-    ret, jpg = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 40])
-    print(jpg.shape)
-    d = jpg.flatten()
-    frame_byte = d.tostring()
-    # jpg_recover = np.fromstring(frame_byte, dtype=np.uint8)
-    # frame_recover = cv2.imdecode(jpg_recover, cv2.IMREAD_COLOR)
+    if frame is not None:
+        ret, jpg = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), quality])
+        # print(jpg.shape)
+        d = jpg.flatten()
+        frame_byte = d.tostring()
+        # jpg_recover = np.fromstring(frame_byte, dtype=np.uint8)
+        # frame_recover = cv2.imdecode(jpg_recover, cv2.IMREAD_COLOR)
 
-    for i in range(spilit):
-        s.sendto(frame_byte[i*packet_size:(i+1)*packet_size], addr)
+        for i in range(spilit):
+            s.sendto(frame_byte[i*packet_size:(i+1)*packet_size], addr)
 
     # cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
     # cv2.resizeWindow('frame', 1280, 720)
     # cv2.imshow('frame', frame)
 
-    recv_cli, recv_addr = server.accept()
-    print("Accept %s %d" % (addr[0], addr[1]))
-    client_handler = threading.Thread(target=handle_client, args=(recv_cli))
-    client_handler.start()
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     break
+    #
+    # cap.release()
+    #
+    # cv2.destroyAllWindows()
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
-cap.release()
 
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
